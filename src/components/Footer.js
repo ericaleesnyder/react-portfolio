9ec8e7d030fca44a860a6c9908f376bf025ebd5f
Erica Snyder <ericaleesnyder@gmail.com>
2021-11-23 21:09:20 -0800

    added footer, styling, and readme

---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <div className="footer mt-auto py-3 es-footer">
+      <div className="container">
+      <a href="https://github.com/ericaleesnyder">Github</a>
+      <br></br>
+      <a href="https://www.linkedin.com/in/ericaleesnyder">LinkedIn</a>
+    </div>
+    </div>
+  );
+}
+
+export default Footer;
--- src/components/NavTabs.js
+++ src/components/NavTabs.js
@@ -2,54 +2,53 @@ import React from 'react';
 
 function NavTabs({ currentPage, handlePageChange }) {
   return (
- 
-      <nav className="navbar nav-style">
-        <div className="container-fluid">
-          <ul className="nav nav-pills">
-            <li className="nav-item">
-              <a
-                href="#about"
-                onClick={() => handlePageChange('About')}
-                
-                className={currentPage === 'About' ? 'nav-link active es-active' : 'nav-link es-inactive'}
-                >
-                About Me
-              </a>
-            </li>
-            <li className="nav-item">
-              <a
-                href="#portfolio"
-                onClick={() => handlePageChange('Portfolio')}
-                
-                className={currentPage === 'Portfolio' ? 'nav-link active es-active' : 'nav-link es-inactive'}
-                >
-                Portfolio
-              </a>
-            </li>
-            <li className="nav-item">
-              <a
-                href="#resume"
-                onClick={() => handlePageChange('Resume')}
-                
-                className={currentPage === 'Resume' ? 'nav-link active es-active' : 'nav-link es-inactive'}
-                >
-                Resume
-              </a>
-            </li>
-            <li className="nav-item">
-              <a
-                href="#contact"
-                
-                onClick={() => handlePageChange('Contact')}
-                className={currentPage === 'Contact' ? 'nav-link active es-active' : 'nav-link es-inactive'}
-                >
-                Contact
-              </a>
-            </li>
-          </ul>
-        </div>
-      </nav>
-    
+    <nav className="navbar nav-style">
+      <div className="container-fluid">
+        <div className="navbar-brand">Erica Snyder</div>
+        <ul className="nav nav-pills">
+          <li className="nav-item">
+            <a
+              href="#about"
+              onClick={() => handlePageChange('About')}
+              
+              className={currentPage === 'About' ? 'nav-link active es-active' : 'nav-link es-inactive'}
+              >
+              About Me
+            </a>
+          </li>
+          <li className="nav-item">
+            <a
+              href="#portfolio"
+              onClick={() => handlePageChange('Portfolio')}
+              
+              className={currentPage === 'Portfolio' ? 'nav-link active es-active' : 'nav-link es-inactive'}
+              >
+              Portfolio
+            </a>
+          </li>
+          <li className="nav-item">
+            <a
+              href="#resume"
+              onClick={() => handlePageChange('Resume')}
+              
+              className={currentPage === 'Resume' ? 'nav-link active es-active' : 'nav-link es-inactive'}
+              >
+              Resume
+            </a>
+          </li>
+          <li className="nav-item">
+            <a
+              href="#contact"
+              
+              onClick={() => handlePageChange('Contact')}
+              className={currentPage === 'Contact' ? 'nav-link active es-active' : 'nav-link es-inactive'}
+              >
+              Contact
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
   );
 }
 
