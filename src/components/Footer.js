f92971b6e4f1539a9d6fc5abfcd9148263c0d31a
Erica Snyder <ericaleesnyder@gmail.com>
2022-01-13 14:42:57 -0800

    fixed target blank for footer links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,10 +8,10 @@ function Footer() {
       <Container>
         <Row className="justify-content-md-center">
           <Col xs="2">
-            <a href="https://github.com/ericaleesnyder" className='btn btn-primary' style={{width:"100%"}}><Github></Github> GitHub</a>
+            <a href="https://github.com/ericaleesnyder" className='btn btn-primary' style={{width:"100%"}} target="_blank" rel="noopener noreferrer"><Github></Github> GitHub</a>
           </Col>
           <Col xs="2">
-            <a href="https://www.linkedin.com/in/ericaleesnyder" className='btn btn-primary' style={{width:"100%"}}><Linkedin></Linkedin> LinkedIn</a>
+            <a href="https://www.linkedin.com/in/ericaleesnyder" className='btn btn-primary' style={{width:"100%"}} target="_blank" rel="noopener noreferrer"><Linkedin></Linkedin> LinkedIn</a>
           </Col>
         </Row>
     </Container>
